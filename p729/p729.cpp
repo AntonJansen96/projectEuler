fabf897f8b798bea92098bfe52f0e571823ea719
Anton Jansen <antonjansen96@gmail.com>
2025-06-15 17:31:50 +0200

    cleanup the code

--- p729/p729.cpp
+++ p729/p729.cpp
@@ -1,14 +1,9 @@
-#include <string.h>
-#include <stdlib.h>
 #include <flint/arb_poly.h>
 #include <flint/arb_calc.h>
-#include <flint/acb.h>
-#include <flint/acb_hypgeom.h>
 
 slong eval_count = 0;
 
-int
-sin_x(arb_ptr out, const arb_t inp, void * params, slong order, slong prec)
+int sin_x(arb_ptr out, const arb_t inp, void * params, slong order, slong prec)
 {
     int xlen = FLINT_MIN(2, order);
 
@@ -22,8 +17,7 @@ sin_x(arb_ptr out, const arb_t inp, void * params, slong order, slong prec)
     return 0;
 }
 
-int
-sin_x2(arb_ptr out, const arb_t inp, void * params, slong order, slong prec)
+int sin_x2(arb_ptr out, const arb_t inp, void * params, slong order, slong prec)
 {
     arb_ptr x;
 
@@ -45,8 +39,7 @@ sin_x2(arb_ptr out, const arb_t inp, void * params, slong order, slong prec)
     return 0;
 }
 
-int
-sin_1x(arb_ptr out, const arb_t inp, void * params, slong order, slong prec)
+int sin_1x(arb_ptr out, const arb_t inp, void * params, slong order, slong prec)
 {
     arb_ptr x;
     int xlen = FLINT_MIN(2, order);
@@ -66,67 +59,6 @@ sin_1x(arb_ptr out, const arb_t inp, void * params, slong order, slong prec)
     return 0;
 }
 
-int
-airy(arb_ptr out, const arb_t inp, void * params, slong order, slong prec)
-{
-    acb_t t, u;
-    int which = ((int *) params)[0];
-    int xlen = FLINT_MIN(2, order);
-
-    acb_init(t);
-    acb_init(u);
-    acb_set_arb(t, inp);
-
-    if (xlen == 1)
-    {
-        if (which == 0)
-            acb_hypgeom_airy(t, NULL, NULL, NULL, t, prec);
-        else if (which == 1)
-            acb_hypgeom_airy(NULL, t, NULL, NULL, t, prec);
-        else if (which == 2)
-            acb_hypgeom_airy(NULL,  NULL, t, NULL, t, prec);
-        else
-            acb_hypgeom_airy(NULL,  NULL, NULL, t, t, prec);
-
-        arb_set(out, acb_realref(t));
-    }
-    else
-    {
-        if (which == 0 || which == 1)
-            acb_hypgeom_airy(t, u, NULL, NULL, t, prec);
-        else
-            acb_hypgeom_airy(NULL, NULL, t, u, t, prec);
-
-        if (which == 0 || which == 2)
-        {
-            arb_set(out + 0, acb_realref(t));
-            arb_set(out + 1, acb_realref(u));
-
-            /* f''(z) = z f(z) */
-            if (xlen == 3)
-                arb_mul(out + 2, out + 0, inp, prec);
-        }
-        else
-        {
-            arb_set(out + 0, acb_realref(u));
-            arb_mul(out + 1, acb_realref(t), inp, prec);
-
-            /* f'''(z) = f(z) + z f'(z) */
-            if (xlen == 3)
-            {
-                arb_mul(out + 2, out + 0, inp, prec);
-                arb_add(out + 2, out + 2, acb_realref(t), prec);
-            }
-        }
-    }
-
-    acb_clear(t);
-    acb_clear(u);
-
-    eval_count++;
-    return 0;
-}
-
 int main(int argc, char *argv[])
 {
     arf_interval_ptr blocks;
@@ -134,112 +66,29 @@ int main(int argc, char *argv[])
     int * info;
     void * params;
     int param1;
-    slong digits, low_prec, high_prec, i, num, found_roots, found_unknown;
-    slong maxdepth, maxeval, maxfound;
-    int refine;
-    double a, b;
+    slong num;
     arf_t C;
     arf_interval_t t, interval;
     arb_t v, w, z;
 
-    if (argc < 4)
-    {
-        flint_printf("real_roots function a b [-refine d] [-verbose] "
-            "[-maxdepth n] [-maxeval n] [-maxfound n] [-prec n]\n");
-        flint_printf("available functions:\n");
-        flint_printf("  1  sin(x)\n");
-        flint_printf("  2  sin(x^2)\n");
-        flint_printf("  3  sin(1/x)\n");
-        flint_printf("  4  Ai(x), Airy function\n");
-        flint_printf("  5  Ai'(x), Airy function\n");
-        flint_printf("  6  Bi(x), Airy function\n");
-        flint_printf("  7  Bi'(x), Airy function\n");
-        return 1;
-    }
-
     param1 = 0;
     params = &param1;
 
-    switch (atoi(argv[1]))
-    {
-        case 1:
-            function = sin_x;
-            break;
-        case 2:
-            function = sin_x2;
-            break;
-        case 3:
-            function = sin_1x;
-            break;
-        case 4:
-            function = airy;
-            param1 = 0;
-            break;
-        case 5:
-            function = airy;
-            param1 = 1;
-            break;
-        case 6:
-            function = airy;
-            param1 = 2;
-            break;
-        case 7:
-            function = airy;
-            param1 = 3;
-            break;
-        default:
-            flint_printf("require a function 0-7\n");
-            return 1;
-    }
+    function = sin_x2;      // function
+    double a = 0;           // interval a
+    double b = 10;          // interval b
 
-    a = atof(argv[2]);
-    b = atof(argv[3]);
+    int refine = 0;
+    slong digits = 0;
+    slong maxdepth = 30;
+    slong maxeval = 100000;
+    slong maxfound = 100000;
 
-    if (a >= b)
-    {
-        flint_printf("require a < b!\n");
-        return 1;
-    }
+    slong low_prec = 30;
+    slong high_prec = digits * 3.32192809488736 + 10;
 
-    refine = 0;
-    digits = 0;
-    maxdepth = 30;
-    maxeval = 100000;
-    maxfound = 100000;
-    low_prec = 30;
-
-    for (i = 4; i < argc; i++)
-    {
-        if (!strcmp(argv[i], "-refine"))
-        {
-            refine = 1;
-            digits = atol(argv[i+1]);
-        }
-        // else if (!strcmp(argv[i], "-verbose"))
-        // {
-        //     arb_calc_verbose = 1;
-        // }
-        else if (!strcmp(argv[i], "-maxdepth"))
-        {
-            maxdepth = atol(argv[i+1]);
-        }
-        else if (!strcmp(argv[i], "-maxeval"))
-        {
-            maxeval = atol(argv[i+1]);
-        }
-        else if (!strcmp(argv[i], "-maxfound"))
-        {
-            maxfound = atol(argv[i+1]);
-        }
-        else if (!strcmp(argv[i], "-prec"))
-        {
-            low_prec = atol(argv[i+1]);
-        }
-    }
-
-    high_prec = digits * 3.32192809488736 + 10;
-    found_roots = 0;
-    found_unknown = 0;
+    slong found_roots = 0;
+    slong found_unknown = 0;
 
     arf_init(C);
     arf_interval_init(t);
@@ -252,22 +101,14 @@ int main(int argc, char *argv[])
     arf_set_d(&interval->b, b);
 
     flint_printf("interval: "); arf_interval_printd(interval, 15); flint_printf("\n");
-    flint_printf("maxdepth = %wd, maxeval = %wd, maxfound = %wd, low_prec = %wd\n",
-        maxdepth, maxeval, maxfound, low_prec);
+    flint_printf("maxdepth = %wd, maxeval = %wd, maxfound = %wd, low_prec = %wd\n", maxdepth, maxeval, maxfound, low_prec);
 
-    num = arb_calc_isolate_roots(&blocks, &info, function,
-        params, interval, maxdepth, maxeval, maxfound, low_prec);
+    num = arb_calc_isolate_roots(&blocks, &info, function, params, interval, maxdepth, maxeval, maxfound, low_prec);
 
-    for (i = 0; i < num; i++)
+    for (slong i = 0; i < num; i++)
     {
         if (info[i] != 1)
         {
-            // if (arb_calc_verbose)
-            // {
-            //     flint_printf("unable to count roots in ");
-            //     arf_interval_printd(blocks + i, 15);
-            //     flint_printf("\n");
-            // }
             found_unknown++;
             continue;
         }
@@ -277,40 +118,22 @@ int main(int argc, char *argv[])
         if (!refine)
             continue;
 
-        if (arb_calc_refine_root_bisect(t,
-            function, params, blocks + i, 5, low_prec)
-            != ARB_CALC_SUCCESS)
+        if (arb_calc_refine_root_bisect(t, function, params, blocks + i, 5, low_prec) != ARB_CALC_SUCCESS)
         {
             flint_printf("warning: some bisection steps failed!\n");
         }
 
-        // if (arb_calc_verbose)
-        // {
-        //     flint_printf("after bisection 1: ");
-        //     arf_interval_printd(t, 15);
-        //     flint_printf("\n");
-        // }
-
-        if (arb_calc_refine_root_bisect(blocks + i,
-            function, params, t, 5, low_prec)
-            != ARB_CALC_SUCCESS)
+        if (arb_calc_refine_root_bisect(blocks + i, function, params, t, 5, low_prec) != ARB_CALC_SUCCESS)
         {
             flint_printf("warning: some bisection steps failed!\n");
         }
-
-        // if (arb_calc_verbose)
-        // {
-        //     flint_printf("after bisection 2: ");
-        //     arf_interval_printd(blocks + i, 15);
-        //     flint_printf("\n");
-        // }
 
         arf_interval_get_arb(v, t, high_prec);
         arb_calc_newton_conv_factor(C, function, params, v, low_prec);
 
         arf_interval_get_arb(w, blocks + i, high_prec);
-        if (arb_calc_refine_root_newton(z, function, params,
-            w, v, C, 10, high_prec) != ARB_CALC_SUCCESS)
+
+        if (arb_calc_refine_root_newton(z, function, params, w, v, C, 10, high_prec) != ARB_CALC_SUCCESS)
         {
             flint_printf("warning: some newton steps failed!\n");
         }
@@ -324,8 +147,8 @@ int main(int argc, char *argv[])
     flint_printf("Found roots: %wd\n", found_roots);
     flint_printf("Subintervals possibly containing undetected roots: %wd\n", found_unknown);
     flint_printf("Function evaluations: %wd\n", eval_count);
-
-    for (i = 0; i < num; i++)
+    
+    for (slong i = 0; i < num; i++)
         arf_interval_clear(blocks + i);
 
     flint_free(blocks);
