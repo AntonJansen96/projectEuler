401e878585af26a6238008f82753f73db4336288
Anton Jansen <antonjansen96@gmail.com>
2025-06-16 14:58:08 +0200

    root finding now works correct sinx2

--- p729/p729.cpp
+++ p729/p729.cpp
@@ -18,10 +18,6 @@ class RootFinder
     arb_calc_func_t d_func;     // 3. function to find root for.
     void *d_param;              // 4. Function parameter(s).
     arf_interval_t d_interval;  // 5. interval for searching the roots.
-    size_t d_maxdepth;          // 6. Max #recursive subdivisions attempted.
-    size_t d_maxeval;           // 7. Max #tested subintervals.
-    size_t d_maxfound;          // 8. Max found number of roots.
-    size_t d_prec;              // 9. Precision used for evaluation.
 
     public:
         // Constructor.
@@ -35,6 +31,8 @@ class RootFinder
         void compute_intervals();
         // Refine interval using bisection.
         void refine_bisect();
+        // Find the actual root using Newton's method.
+        void refine_newton();
         // Print the intervals (midpoints).
         void printintervals();
         // Provide summary.
@@ -51,11 +49,8 @@ class RootFinder
 inline RootFinder::RootFinder(size_t P, double a, double b)
 :
     d_func(sin_x2),
-    d_param(&P),
-    d_maxdepth(20),
-    d_maxeval(100000),
-    d_maxfound(LONG_MAX),
-    d_prec(30)
+    // d_func(myfunc),
+    d_param(&P)
 {
     arf_interval_init(d_interval);
     arf_set_d(&d_interval->a, a);
@@ -81,19 +76,25 @@ inline void RootFinder::run()
     timer.start();
 
     this->compute_intervals();
-    // this->printintervals();
     this->refine_bisect();
     this->printintervals();
+    this->refine_newton();
 
     this->summary();
-    print(fs("\nRootFinder took {}", timer));
+    print(fs("RootFinder took: {}", timer));
 }
 
 // Compute intervals.
 inline void RootFinder::compute_intervals()
 {
-    d_num = arb_calc_isolate_roots(&d_blocks, &d_flags, d_func, d_param, d_interval, d_maxdepth, d_maxeval, d_maxfound, d_prec);
-    
+    // Tunable parameters.
+    size_t const maxdepth = 20;         // Max #recursive subdivisions attempted (20-100).
+    size_t const maxeval = 1'000'000;   // Max #tested subintervals (10'000 - 1'000'000).
+    size_t const maxfound = LONG_MAX;   // Max found number of roots.
+    size_t const prec = 64;             // Precision used during evaluation (30-200 bits).
+
+    d_num = arb_calc_isolate_roots(&d_blocks, &d_flags, d_func, d_param, d_interval, maxdepth, maxeval, maxfound, prec);
+
     for (size_t idx = 0; idx != d_num; ++idx)
     {
         if (d_flags[idx] != 1)
@@ -106,17 +107,49 @@ inline void RootFinder::compute_intervals()
 // Refine interval using bisection.
 inline void RootFinder::refine_bisect()
 {
-    size_t const iter = 10;
-     for (size_t idx = 0; idx != d_num; ++idx)
-        if (arb_calc_refine_root_bisect(d_blocks + idx, d_func, d_param, d_blocks + idx, iter, d_prec) != ARB_CALC_SUCCESS)
+    // Tunable parameters.
+    size_t const iter = 10;   // Number of bisection steps per interval (5-100).
+    size_t const prec = 64;   // Working precision for interval arithmetic (30-200 bits).
+
+    for (size_t idx = 0; idx != d_num; ++idx)
+        if (arb_calc_refine_root_bisect(d_blocks + idx, d_func, d_param, d_blocks + idx, iter, prec) != ARB_CALC_SUCCESS)
             print(fs("WARNING: refine_bisect failed for root interval #{}", idx + 1));
+}
+
+inline void RootFinder::refine_newton()
+{
+    // Tunable parameters.
+    size_t const eval_extra_prec = 5;   // Extra prec to use internally during calc.
+    size_t const prec1 = 64;            // Precision used when converting (30-200 bits).
+    size_t const prec2 = 200;           // Target Precision of after Newton (64-500 bits).
+
+    arb_t root;
+    arb_init(root);
+    arf_t conv_factor;
+    arf_init(conv_factor);
+
+    for (size_t idx = 0; idx != d_num; ++idx)
+    {
+        arf_interval_get_arb(root, d_blocks + idx, prec1);
+        arb_calc_newton_conv_factor(conv_factor, d_func, d_param, root, prec1);
+
+        if (arb_calc_refine_root_newton(root, d_func, d_param, root, root, conv_factor, eval_extra_prec, prec2) != ARB_CALC_SUCCESS)
+            print(fs("WARNING: refine_newton failed for root interval #{}", idx + 1));
+        else
+            print(fs("{}. {}", idx + 1, arb_get_str(root, prec2, 0)));
+    }
+
+    arb_clear(root);
+    arf_clear(conv_factor);
 }
 
 // Print the intervals (midpoints).
 inline void RootFinder::printintervals()
 {
+    // Tunable parameters.
     size_t const digits = 10;
-    
+
+    arb_t d_temp;
     arb_init(d_temp);
 
     for (size_t idx = 0; idx != d_num; ++idx)
@@ -131,31 +164,6 @@ inline void RootFinder::printintervals()
 
     arb_clear(d_temp);
 }
-
-// inline void RootFinder::refine_bisect()
-// {
-    // Refines root interval entry idx in d_blocks array using bisection.
-    // size_t const iter = 10;
-    // arb_calc_refine_root_bisect(d_blocks + idx, myfunc, d_param, d_blocks + idx, iter, d_prec);
-
-    // // Refines root interval entry idx in d_block array using Newton's method.
-    // arb_t root;
-    // arb_init(root);
-    // arf_interval_get_arb(root, d_blocks + idx, d_prec); // Root now holds starting midpoint.
-
-    // arf_t conv_factor;
-    // arf_init(conv_factor);
-    // arb_calc_newton_conv_factor(conv_factor, myfunc, d_param, root, d_prec); // Conv factor now holds conversion factor.
-
-    // if (arb_calc_refine_root_newton(root, myfunc, d_param, root, root, conv_factor, 10, d_prec) == ARB_CALC_SUCCESS)
-    // {
-    //     print("Newton's method ran succesfully");
-    //     print(arb_get_str(root, 50, 0));
-    // }
-
-    // arb_clear(root);
-    // arf_clear(conv_factor);
-// }
 
 // Provide summary.
 inline void RootFinder::summary()
@@ -219,4 +227,5 @@ inline int RootFinder::myfunc(arb_ptr out, const arb_t inp, void *param, slong o
 int main()
 {
     RootFinder(3, 0.001, 7.0).run();
+    // RootFinder(3, -2, 2).run(); // Project Euler
 }
